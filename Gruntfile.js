04798455a50404a98293eaaabe31f945a2461d38
ele0 <ele0@users.noreply.github.com>
2016-04-12 03:41:23 -0400

    Refactor to separate  src and dist.  - included optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,7 @@ module.exports = function(grunt) {
       dev: {
         options: {
           engine: 'im',
-          sizes: [{
-            width: 1200,
-            quality: 70
-          },
+          sizes: [
           {
             width: 1024,
             quality: 60
@@ -21,6 +18,10 @@ module.exports = function(grunt) {
           {
             width: 500,
             quality: 60
+          },
+          {
+            width: 100,
+            quality: 60
           }]
         },
 
@@ -31,8 +32,8 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
-          cwd: 'views/img_src/',
-          dest: 'views/images/'
+          cwd: 'src/views/images/',
+          dest: 'src/views/images/precompressed/'
         }]
       }
     },
@@ -40,7 +41,7 @@ module.exports = function(grunt) {
     /* Clear out the images directory if it exists */
     clean: {
       dev: {
-        src: ['views/images'],
+        src: ['dist/*', 'src/views/images/precompressed/*'],
       },
     },
 
@@ -48,21 +49,111 @@ module.exports = function(grunt) {
     mkdir: {
       dev: {
         options: {
-          create: ['views/images']
+          create: ['dist/views/images', 'dist/img']
         },
       },
     },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    /* Copy everything into dist except images from view */
     copy: {
       dev: {
+        files: [
+        // copy files
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['*/*',  '!**/views/images/**', '!**/img/**'],
+            dest: 'dist/',
+            filter: 'isFile',
+            noProcess: ''
+          }]
+      },
+      main: {
+        files:[
+        {/* Copy the "fixed" images that don't go through processing into the images/directory and everything into dist*/
+            expand: true,
+            src: 'src/views/images/fixed/*.{gif,jpg,png,svg}',
+            dest: 'src/views/images/precompressed/',
+            flatten: true,
+          }]
+      }
+    },
+
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'src/views/images/precompressed/',
+            src: ['**/*.png'],
+            // Could also match cwd line above.
+            dest: 'dist/views/images/',
+            ext: '.png'
+          },
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'src/img/',
+            src: ['**/*.png'],
+            // Could also match cwd line above.
+            dest: 'dist/img/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'src/views/images/precompressed/',
+            src: ['**/*.jpg'],
+            // Could also match cwd.
+            dest: 'dist/views/images/',
+            ext: '.jpg'
+          },
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'src/img/',
+            src: ['**/*.jpg'],
+            // Could also match cwd.
+            dest: 'dist/img/',
+            ext: '.jpg'
+          }
+        ]
+      }
+    },
+
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
         files: [{
-          expand: true,
-          src: 'views/img_src/fixed/*.{gif,jpg,png,svg}',
-          dest: 'views/images/',
-          flatten: true,
+         expand: true,
+         cwd: 'src',
+         src: '**/*.html',
+         dest: 'dist/'
         }]
       },
+      dev: {                                       // Another target
+        files: {
+          'dist/index.html': 'src/index.html'
+        }
+      }
     },
 
     /* Import info from package.json
@@ -82,6 +173,8 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -90,7 +183,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // default tasks
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy:main','responsive_images', 'imagemin', 'copy:dev', 'htmlmin:dist']);
 
 
 };
