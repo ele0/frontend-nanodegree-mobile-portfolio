766329c2f47f49b48f75ddf0def97c342629b735
ele0 <ele0@users.noreply.github.com>
2016-04-21 00:14:11 -0400

    Optimize other html pages

--- views/js/main.js
+++ views/js/main.js
@@ -503,6 +503,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName("mover"); //document.querySelectorAll('.mover');
+
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
