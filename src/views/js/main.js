d047c75a7a400588e857df1d051cffa6f64886ac
ele0 <ele0@users.noreply.github.com>
2016-04-21 01:49:08 -0400

    Optimize js to get close to 60fps

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -502,10 +502,17 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  /* changed way of accessing elements with a particular class to be more efficient */
   var items = document.getElementsByClassName("mover"); //document.querySelectorAll('.mover');
 
+  /* Moved the calculation of the position of the body element from the top out of the for loop because the value is
+  approximately constant. */
+  var topDist = document.body.scrollTop / 1250;
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(topDist + (i % 5));
+    //console.log(phase, (document.body.scrollTop / 1250 + (i % 5)));
+
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
